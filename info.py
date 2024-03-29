02aca6a0dd0f8863ebcfe68a6d125bd4e18b0289
Mismukta1321 <155456446+Mismukta1321@users.noreply.github.com>
2024-03-29 15:17:03 +0600

    Update info.py

--- info.py
+++ info.py
@@ -44,8 +44,8 @@ NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', " mongodb+srv://muktaakhter1321:muktaakhter1321@cluster0.wkebqmb.mongodb.net/?retryWrites=true&w=majority
 ")
-DATABASE_NAME = environ.get('DATABASE_NAME', "vjbotsaban")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'vjbots')
+DATABASE_NAME = environ.get('DATABASE_NAME', "CTGMOVIEBOT")
+COLLECTION_NAME = environ.get('COLLECTION_NAME', 'CTGMOVIE')
 
 # Others
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'moneykamalo.com')
